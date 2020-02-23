1dea06d45a2348daec149a4415c48ae6fa8812b5
evansmith-github <esmith40@student.mtsac.edu>
2020-02-22 18:00:42 -0800

    added weather zip input

--- src/WeatherForm.js
+++ src/WeatherForm.js
@@ -4,10 +4,16 @@ import React, { Component } from 'react';
 class WeatherForm extends Component {
     
     render() {
-        return (
-            <div>
+        return ( 
+            <div className="WeatherForm-wrapper">
+            <div className="WeatherForm-input">
+                <input onChange={this.props.handleInputChange}></input>
+            </div>
+            <div className="WeatherForm-submit">
                 <button onClick={this.props.handleSubmit}>Click</button>
             </div>
+            </div>
+
         )
     }
 }
