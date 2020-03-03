8530d58f271f3952c4b138b00927c812e450af9b
evansmith-github <esmith40@student.mtsac.edu>
2020-03-03 15:53:12 -0800

    updated food query generator

--- src/GenerateQuery.js
+++ src/GenerateQuery.js
@@ -5,13 +5,13 @@ import {coldFoods, neutralFoods, hotFoods} from './Foods.js';
 const GenerateQuery = (temp) => {
     if(temp <= 65) {
         let randIndex =  Math.floor(Math.random() * hotFoods.length);
-        return hotFoods[randIndex];
+        return coldFoods[randIndex];
     } else if(temp <= 80) {
         let randIndex = Math.floor(Math.random() * neutralFoods.length);
         return neutralFoods[randIndex];
     } else {
         let randIndex = Math.floor(Math.random() * coldFoods.length);
-        return coldFoods[randIndex];
+        return hotFoods[randIndex];
     }
 }
 
